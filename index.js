a85913a45b9955bc29835e82d76d8682bc01a92f
Seth Lakowske <lakowske@gmail.com>
2015-01-15 20:44:37 -0600

    now keeps deployer secret in a config file

--- index.js
+++ index.js
@@ -2,16 +2,39 @@
  * (C) 2014 Seth Lakowske
  */
 
-
+var fs               = require('fs');
 var http             = require('http');
 var httpProxy        = require('http-proxy');
 var proxyByDirectory = require('proxy-by-directory');
+var Deployer         = require('github-webhook-deployer');
 
-var proxy = httpProxy.createProxyServer({});
+
+var port   = parseInt(process.argv[2], 10);
+
+if (!port) {
+    console.log('invalid port\n');
+    console.log('Usage: node index.js <port>');
+    process.exit(-1);
+}
+
+var proxy  = httpProxy.createProxyServer({});
 
 var server = http.createServer(proxyByDirectory({
     '/articles' : { target : 'http://localhost:4444/' },
     '/' : { target : 'http://sethlakowske.com' }
 }, proxy))
 
-server.listen(8880);
+console.log('listening on ' + port);
+
+server.listen(port);
+
+try {
+    var config = JSON.parse(fs.readFileSync('config.json'));
+} catch (error) {
+    console.log(error);
+    console.log('Problem loading configuration. Create a config.json file with deployer configuration');
+    process.exit(-1);
+}
+
+var deployer = new Deployer(config);
+deployer.listen(port+1);
