60395b33299c50da864f9a4c1d0b7ef064cb3ad5
Seth Lakowske <lakowske@gmail.com>
2015-02-09 19:40:03 -0600

    log requests

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var fs               = require('fs');
 var http             = require('http');
 var httpProxy        = require('http-proxy');
 var proxyByDirectory = require('proxy-by-directory');
+var level            = require('level');
+var logger           = require('http-request-logger');
+var router           = require('routes')();
 var Deployer         = require('github-webhook-deployer');
 
 var port   = parseInt(process.argv[2], 10);
@@ -16,14 +19,31 @@ if (!port) {
     process.exit(-1);
 }
 
+//open the request db
+var db = level('./request.db');
+requestLogger = logger(db);
+request       = requestLogger.request();
+router.addRoute('/requests', requestLogger.requests());
+
 var proxy  = httpProxy.createProxyServer({});
 
-var server = http.createServer(proxyByDirectory({
+var proxyFn = proxyByDirectory({
     '/articles' : { target : 'http://localhost:5555/' },
     '/static' : { target : 'http://localhost:5555/' },
     '/requests' : { target : 'http://localhost:5555/' },
     '/' : { target : 'http://sethlakowske.com:7777' }
-}, proxy))
+}, proxy)
+
+var server = http.createServer(function(req, res) {
+    //log the request
+    request(req, res);
+
+    var m = router.match(req.url);
+    if (m) m.fn(req, res, m.params); //check if we should serve the request
+    else proxyFn(req, res);          //otherwise route the request to the correct server
+
+
+});
 
 console.log('proxy listening on ' + port);
 
